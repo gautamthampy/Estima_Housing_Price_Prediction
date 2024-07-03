f06b4a68aa03dc4e3b05343146539b84cc13c183
Gautam Thampy <thampygautam@gmail.com>
2024-07-03 15:19:44 +0530

    prediction api using flask

--- app.py
+++ app.py
@@ -1,32 +1,32 @@
 from flask import Flask, request, jsonify
-import utilities
+import util
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def greetings():
-    return 'Welcome to Estima Server'
+@app.route('/',methods=['GET'])
+def hello_world():
+    return 'Welcome to Bangalore Housing Price Fetcher Server🤳'
 
-@app.route('/getLocation', methods=['GET'])
-def getLocation():
-    response = jsonify({'location':utilities.getLocation()})
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
-
-@app.route('/getPredictedPrice', methods=['GET'])
-def getPredictedPrice():
-    area = float(request.form['total_sqft'])
-    bath = int(request.form['bath'])
-    bhk = int(request.form['bhk'])
-    location = request.form['location']
-    response = jsonify({
-        'estimated_price':utilities.getPredictedPrice(area, bath, bhk, location)
-    })
+@app.route('/getlocation',methods=['GET'])
+def getlocation():
+    response=jsonify({'location':util.getlocations()})
     response.headers.add('Access-Control-Allow-Origin','*')
     return response
 
+@app.route('/getpredictedprice', methods=['POST'])
+def getpredictedprice():
+    total_sqft=float(request.form['total_sqft'])
+    bath=int(request.form['bath'])
+    bhk=int(request.form['bhk'])
+    location=request.form['location']
+    response=jsonify({
+        'estimated_price':util.getpredictedprice(total_sqft,bath,bhk,location)
+    })   
+    response.headers.add('Access-Control-Allow-Origin','*')
+    return response 
+
 
 if __name__=="__main__":
-    print("starting Estima server.....")
-    utilities.loadArtifacts()
-    app.run()             
+    print("starting flask server for bangalore apartment price prediction.....")
+    util.loadsavedartifacts()
+    app.run()
